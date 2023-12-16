c5d54e2e9e00673295417e134f3a7f4886ec2dd2
pawles <268419@student.pwr.edu.pl>
2023-12-16 13:49:13 +0100

    Add vector quantization for list5

--- lista5/quantization/src/lib.rs
+++ lista5/quantization/src/lib.rs
@@ -1,11 +1,105 @@
 use std::fs;
-use pic_entropy::colour::{Colour, colour_to_bytes};
+use std::cmp::Ordering;
+use rand::distributions::{Distribution, Uniform};
+use pic_entropy::colour::*;
 use crate::colour_dict::ColourDict;
 
 pub mod colour_dict;
 
-type PixelArray = Vec<Vec<Colour>>;
 type PixelVec = Vec<Colour>;
+
+#[derive(Clone)]
+#[derive(Debug)]
+pub struct Centroid {
+    blue: f64,
+    green: f64,
+    red: f64,
+}
+
+type CentroidVec = Vec<Centroid>;
+
+fn lbg_move_centroids(centroids: &mut CentroidVec, clusters: &Vec<PixelVec>) {
+    for i in 0..centroids.len() {
+        if clusters[i].len() > 0 {
+            centroids[i].blue = extract_colour(&clusters[i], &Hue::BLUE).iter().map(|&v| v as usize).sum::<usize>() as f64 / clusters[i].len() as f64;
+            centroids[i].green = extract_colour(&clusters[i], &Hue::GREEN).iter().map(|&v| v as usize).sum::<usize>() as f64 / clusters[i].len() as f64;
+            centroids[i].red = extract_colour(&clusters[i], &Hue::RED).iter().map(|&v| v as usize).sum::<usize>() as f64 / clusters[i].len() as f64;
+            println!("moved: {:?}", centroids[i]);
+        } else {
+            centroids[i].blue = 1000.0;
+            centroids[i].green = 1000.0;
+            centroids[i].red = 1000.0;
+        }
+    }
+}
+
+fn lbg_cluster(centroids: &CentroidVec, pixels: &PixelVec) -> Vec<PixelVec> {
+    let grouped_pixels = pixels
+        .iter()
+        .map(|p| centroids
+            .iter()
+            .map(|c| (p.blue as f64 - c.blue).abs() + (p.green as f64 - c.green).abs() + (p.red as f64 - c.red).abs())
+            .enumerate()
+            .min_by(|(_, d1), (_, d2)| d1.partial_cmp(d2).unwrap_or(Ordering::Equal))
+            .map(|(idx, _)| idx)
+            .unwrap()
+        ).collect::<Vec<usize>>();
+
+    let mut clusters = Vec::new();
+    clusters.resize(centroids.len(), Vec::new());
+    for i in 0..grouped_pixels.len() {
+        clusters[grouped_pixels[i]].push(pixels[i]);
+    }
+    return clusters;
+}
+
+pub fn create_lbg_dictionary(pixels: &PixelVec, no_bits: u8) -> PixelVec {
+    let mut rng = rand::thread_rng();
+    let noise = Uniform::new_inclusive(-1, 1);
+    let no_colours = 2_i32.pow(no_bits as u32) as usize;
+    let no_pixels = pixels.len();
+
+    let avg_blue = extract_colour(&pixels, &Hue::BLUE).iter().map(|&v| v as usize).sum::<usize>() as f64 / no_pixels as f64;
+    let avg_green = extract_colour(&pixels, &Hue::GREEN).iter().map(|&v| v as usize).sum::<usize>() as f64 / no_pixels as f64;
+    let avg_red = extract_colour(&pixels, &Hue::RED).iter().map(|&v| v as usize).sum::<usize>() as f64 / no_pixels as f64;
+    let avg = Centroid{blue: avg_blue, green: avg_green, red: avg_red};
+
+    let mut centroids = vec![avg];
+    let mut no_centroids = 1;
+    for _ in 0..10 {
+        println!("{:?}", centroids);
+        if no_centroids < no_colours {
+            for j in 0..no_centroids {
+                let new_centroid = centroids[j].clone();
+                centroids.push(new_centroid);
+            }
+            no_centroids *= 2;
+        }
+        for j in 0..no_centroids {
+            centroids[j].blue += noise.sample(&mut rng) as f64;
+            centroids[j].green += noise.sample(&mut rng) as f64;
+            centroids[j].red += noise.sample(&mut rng) as f64;
+        }
+        let clusters = lbg_cluster(&centroids, &pixels);
+        lbg_move_centroids(&mut centroids, &clusters);
+    }
+    return centroids
+        .iter()
+        .map(|c| Colour{blue: c.blue.round() as u8, green: c.green.round() as u8, red: c.red.round() as u8})
+        .collect();
+}
+
+pub fn vector_quantize(pixels: &PixelVec, dictionary: &PixelVec) -> PixelVec {
+    return pixels
+        .iter()
+        .map(|p| dictionary
+            .iter()
+            .min_by_key(|c| p.blue.abs_diff(c.blue) as usize + p.green.abs_diff(c.green) as usize + p.red.abs_diff(c.red) as usize)
+            .cloned()
+            .unwrap()
+        ).collect::<PixelVec>();
+    
+}
 
 pub fn quantize(pixels: &PixelVec, dictionary: &ColourDict) -> PixelVec {
     return pixels
@@ -31,9 +125,37 @@ pub fn quantize(pixels: &PixelVec, dictionary: &ColourDict) -> PixelVec {
         .collect();
 }
 
+pub fn calculate_mse(original: &Vec<u8>, quantized: &Vec<u8>) -> f64 {
+    let no_bytes = original.len();
+    return 1.0 / no_bytes as f64 * (0..no_bytes)
+        .map(|i| original[i].abs_diff(quantized[i]) as f64)
+        .fold(0f64, |acc, diff| acc + diff.powf(2.0));
+}
+
+pub fn calculate_snr(original: &Vec<u8>, mse: f64) -> f64 {
+    let no_bytes = original.len();
+    return 1.0 / no_bytes as f64 / mse * (0..no_bytes)
+        .fold(0f64, |acc, i| acc + (original[i] as f64).powf(2.0))
+}
+
+/*
+pub fn calculate_mse(original: &PixelVec, quantized: &PixelVec) -> f64 {
+    let no_pixels = original.len();
+    return 1.0 / no_pixels as f64 * (0..no_pixels)
+        .map(|i| (original[i].blue.abs_diff(quantized[i].blue) as f64, original[i].green.abs_diff(quantized[i].green) as f64, original[i].red.abs_diff(quantized[i].red) as f64))
+        .fold(0f64, |acc, diff| acc + diff.0.powf(2.0) + diff.1.powf(2.0) + diff.2.powf(2.0))
+}
+
+pub fn calculate_snr(original: &PixelVec, mse: f64) -> f64 {
+    let no_pixels = original.len();
+    return 1.0 / no_pixels as f64 / mse * (0..no_pixels)
+        .fold(0f64, |acc, i| acc + (original[i].blue as f64).powf(2.0) + (original[i].green as f64).powf(2.0) + (original[i].red as f64).powf(2.0));
+}
+*/
+
 pub fn write_tga(out: &str, header: &[u8], pixels: &PixelVec, footer: &[u8]) {
     let mut image_bytes = header.to_vec();
     image_bytes.extend(colour_to_bytes(&pixels));
     image_bytes.extend(footer);
-    fs::write(out, image_bytes);
+    fs::write(out, image_bytes).expect("can't write to file");
 }
--- lista5/quantization/src/main.rs
+++ lista5/quantization/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::error::Error;
 use pic_entropy::read_data;
-use quantization::colour_dict::ColourDict;
+use pic_entropy::colour::*;
 use quantization::*;
 
 fn flatten<T: std::clone::Clone>(v: &Vec<Vec<T>>) -> Vec<T> {
@@ -14,20 +14,39 @@ fn flatten<T: std::clone::Clone>(v: &Vec<Vec<T>>) -> Vec<T> {
 
 fn main() -> Result<(), Box<dyn Error>>{
     let args: Vec<String> = env::args().collect();
-    if args.len() < 6 {
-        eprintln!("usage: path/to/programme <input-file> <output-file> <r-pixels> <g-pixels> <b-pixels>");
+    if args.len() < 4 {
+        eprintln!("usage: path/to/programme <input-file> <output-file> <bits>");
         std::process::exit(1);
     }
 
     let (header, pixels, footer) = read_data(&args[1])?;
     let pixels = pixels.iter().rev().cloned().collect();
     let flattened_pixels = flatten(&pixels);
-    let quantizer_dictionary = ColourDict::new(args[5].parse().unwrap(), args[4].parse().unwrap(), args[3].parse().unwrap());
+    //let quantizer_dictionary = ColourDict::new(args[5].parse().unwrap(), args[4].parse().unwrap(), args[3].parse().unwrap());
+    let quantizer_dictionary = create_lbg_dictionary(&flattened_pixels, args[3].parse().unwrap());
 
     println!("{:?}", quantizer_dictionary);
 
-    let quantized_pixels = quantize(&flattened_pixels, &quantizer_dictionary);
-    
+    let quantized_pixels = vector_quantize(&flattened_pixels, &quantizer_dictionary);
+
+    let mse = calculate_mse(&colour_to_bytes(&flattened_pixels), &colour_to_bytes(&quantized_pixels));
+    let mse_blue = calculate_mse(&extract_colour(&flattened_pixels, &Hue::BLUE), &extract_colour(&quantized_pixels, &Hue::BLUE));
+    let mse_green = calculate_mse(&extract_colour(&flattened_pixels, &Hue::GREEN), &extract_colour(&quantized_pixels, &Hue::GREEN));
+    let mse_red = calculate_mse(&extract_colour(&flattened_pixels, &Hue::RED), &extract_colour(&quantized_pixels, &Hue::RED));
+    println!("mse = {:?}", mse);
+    println!("mse (blue) = {:?}", mse_blue);
+    println!("mse (green) = {:?}", mse_green);
+    println!("mse (red) = {:?}", mse_red);
+
+    let snr = calculate_snr(&colour_to_bytes(&flattened_pixels), mse);
+    let snr_blue = calculate_snr(&extract_colour(&flattened_pixels, &Hue::BLUE), mse);
+    let snr_green = calculate_snr(&extract_colour(&flattened_pixels, &Hue::GREEN), mse);
+    let snr_red = calculate_snr(&extract_colour(&flattened_pixels, &Hue::RED), mse);
+    println!("snr = {:?}", snr);
+    println!("snr (blue) = {:?}", snr_blue);
+    println!("snr (green) = {:?}", snr_green);
+    println!("snr (red) = {:?}", snr_red);
+
     //println!("{:?}", quantized_pixels);
 
     write_tga(&args[2], &header, &quantized_pixels, &footer);
